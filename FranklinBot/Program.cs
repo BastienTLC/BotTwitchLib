49971ea0dfc35cc0db5458df61101cbd61dee6ea
Bastien Talec <bastientalec@orange.fr>
2021-03-17 10:38:32 +0100

    Initialisation

--- FranklinBot/Program.cs
+++ FranklinBot/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace FranklinBot
 {
@@ -6,7 +10,6 @@ namespace FranklinBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
