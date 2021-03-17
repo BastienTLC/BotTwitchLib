fd37e1f42dca165720ee05d36708e7c203f43d99
Bastien Talec <bastientalec@orange.fr>
2021-03-17 10:15:58 +0100

    ajout des fichiers de base

---
+++ FranklinBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FranklinBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
